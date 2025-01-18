77e3c21f3c9ed539f9d303241764d742796afeb7
Joonas Nuolikoski <joonas.nuolikoski@gmail.com>
2025-01-18 09:16:35 +0200

    add tailwind and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NavLink } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-gray-800'>
+      <div className='bg-gradient-to-r from-slate-800 to-slate-700 flex flex-row-reverse px-4 py-1'>
+        <NavLink
+          to='/'
+          className={({ isActive }) => (isActive ? 'isActive link' : 'link')}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to='/settings'
+          className={({ isActive }) => (isActive ? 'isActive link' : 'link')}
+        >
+          Settings
+        </NavLink>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='text-center'>Content here</div>
+    </div>
   )
 }
 
 export default App
+
